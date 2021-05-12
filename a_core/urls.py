e69cb90274fa5b40f7ae4f57c2aa11b7b8ce2429
Oleksandr Danylchenko <oleksandr_danylchenko_cw@bmc.com>
2021-05-12 12:27:00 +0300

    Move dir from shitty previously autogenerated from startproject util

--- a_core/urls.py
+++ a_core/urls.py
@@ -1,4 +1,4 @@
-"""a_core URL Configuration
+"""site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
