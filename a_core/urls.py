7f4b384c08881c169419efe2c342c36d60112da1
Oleksandr Danylchenko <oleksandr_danylchenko_cw@bmc.com>
2021-05-12 13:14:13 +0300

    Fix issues Add app

--- a_core/urls.py
+++ a_core/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('btc_trade_ua/', include('btc_trade_ua.urls')),
+
     path('admin/', admin.site.urls),
 ]
